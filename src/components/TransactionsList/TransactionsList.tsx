88348e6e3c678ff8b75e155719382c189ac5e4da
LarisNicoli <141666428+LarisNicoli@users.noreply.github.com>
2023-11-13 19:17:35 -0300

    ordernação (#13)

--- src/components/TransactionsList/TransactionsList.tsx
+++ src/components/TransactionsList/TransactionsList.tsx
@@ -13,17 +13,19 @@ export const TransactionsList: FC<TransactionsListProps> = ({
     <h1>Lista de transações</h1>
 
     <ol className="flex flex-col gap-3">
-      {transactions.map((transaction) => (
-        <li key={transaction.id}>
-          <TransactionDetail
-            value={transaction.value}
-            type={transaction.type}
-            referenceDate={transaction.referenceDate}
-            id={transaction.id}
-            description={transaction.description}
-          />
-        </li>
-      ))}
+      {transactions
+        .sort((a, b) => b.referenceDate.getTime() - a.referenceDate.getTime())
+        .map((transaction) => (
+          <li key={transaction.id}>
+            <TransactionDetail
+              value={transaction.value}
+              type={transaction.type}
+              referenceDate={transaction.referenceDate}
+              id={transaction.id}
+              description={transaction.description}
+            />
+          </li>
+        ))}
     </ol>
   </>
 );
